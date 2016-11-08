4c64a468b3242368f88115a7f068ab7a875bc30d
newandroidtester <new@new.com>
2016-11-08 22:09:23 +0100

    65_Show Data Inside Adapter From Database(30)

--- app/src/main/java/slidenerd/vivz/bucketdrops/ActivityMain.java
+++ app/src/main/java/slidenerd/vivz/bucketdrops/ActivityMain.java
@@ -4,61 +4,86 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
 import slidenerd.vivz.bucketdrops.adapters.AdapterDrops;
+import slidenerd.vivz.bucketdrops.beans.Drop;
 
-public class ActivityMain extends AppCompatActivity  {
+//TODO add a layout manager for the RecyclerView
+public class ActivityMain extends AppCompatActivity {
 
+    public static final String TAG = "VIVZ";
     Toolbar mToolbar;
     Button mBtnAdd;
     RecyclerView mRecycler;
-
-    private View.OnClickListener mtBtnAddListener = new View.OnClickListener(){
+    Realm mRealm;
+    RealmResults<Drop> mResults;
+    AdapterDrops mAdapter;
+    private View.OnClickListener mBtnAddListener = new View.OnClickListener() {
 
         @Override
-        public void onClick(View view) {
-
+        public void onClick(View v) {
             showDialogAdd();
         }
     };
 
+
+
+    private RealmChangeListener mChangeListener = new RealmChangeListener() {
+        @Override
+        public void onChange(Object element) {
+        Log.d(TAG, "onChange: was called");
+            mAdapter.update(mResults);
+
+        }
+    };
+
     private void showDialogAdd() {
-        //Create an object of a Dialog
         DialogAdd dialog = new DialogAdd();
-        //show the dialog
-        dialog.show(getSupportFragmentManager(),"Add");
-
+        dialog.show(getSupportFragmentManager(), "Add");
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //initialize
+        mRealm = Realm.getDefaultInstance();
+        mResults = mRealm.where(Drop.class).findAllAsync();
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mBtnAdd = (Button) findViewById(R.id.btn_add);
         mRecycler = (RecyclerView) findViewById(R.id.rv_drops);
-        mRecycler.setAdapter(new AdapterDrops(this));
-        mBtnAdd.setOnClickListener(mtBtnAddListener);
-
+        mAdapter = new AdapterDrops(this, mResults);
+        mRecycler.setAdapter(mAdapter);
+        mBtnAdd.setOnClickListener(mBtnAddListener);
         setSupportActionBar(mToolbar);
         initBackgroundImage();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mResults.addChangeListener(mChangeListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mResults.removeChangeListener(mChangeListener);
+    }
+
     private void initBackgroundImage() {
         ImageView background = (ImageView) findViewById(R.id.iv_background);
-        Glide.with(this).load(R.drawable.background)
+        Glide.with(this)
+                .load(R.drawable.background)
                 .centerCrop()
                 .into(background);
     }
-
-
-
-
 }
--- app/src/main/java/slidenerd/vivz/bucketdrops/DialogAdd.java
+++ app/src/main/java/slidenerd/vivz/bucketdrops/DialogAdd.java
@@ -12,13 +12,12 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 
 import io.realm.Realm;
-import io.realm.RealmConfiguration;
 import slidenerd.vivz.bucketdrops.beans.Drop;
 
 /**
- * Created by Lilla és Levi on 2016.09.05..
+ * Created by vivz on 28/12/15.
  */
-public class DialogAdd extends DialogFragment{
+public class DialogAdd extends DialogFragment {
 
     private ImageButton mBtnClose;
     private EditText mInputWhat;
@@ -27,88 +26,50 @@ public class DialogAdd extends DialogFragment{
 
     private View.OnClickListener mBtnClickListener = new View.OnClickListener() {
         @Override
-        public void onClick(View view) {
-            int id = view.getId();
-            dismiss();
+        public void onClick(View v) {
+            int id =v.getId();
             switch (id){
                 case R.id.btn_add_it:
                     addAction();
                     break;
-
             }
+            dismiss();
         }
     };
 
     //TODO process date
     private void addAction() {
-        //get the value of the goal or 'to-do'
+        //get the value of the 'goal' or 'to-do'
         //get the time when it was added
         String what=mInputWhat.getText().toString();
         long now = System.currentTimeMillis();
-
-        RealmConfiguration configuration = new RealmConfiguration.Builder(getActivity()).build();
-        Realm.setDefaultConfiguration(configuration);
-
-        Realm realm = Realm.getDefaultInstance();
-        //0 because of custom widget, false by default
-        Drop drop = new Drop(what, now, 0, false);
+        Realm realm=Realm.getDefaultInstance();
+        Drop drop= new Drop(what, now, 0, false);
         realm.beginTransaction();
         realm.copyToRealm(drop);
         realm.commitTransaction();
         realm.close();
 
-
     }
 
     public DialogAdd() {
-
-
     }
 
-    /**
-     * Called to have the fragment instantiate its user interface view.
-     * This is optional, and non-graphical fragments can return null (which
-     * is the default implementation).  This will be called between
-     * {@link #onCreate(Bundle)} and {@link #onActivityCreated(Bundle)}.
-     * <p>
-     * <p>If you return a View from here, you will later be called in
-     * {@link #onDestroyView} when the view is being released.
-     *
-     * @param inflater           The LayoutInflater object that can be used to inflate
-     *                           any views in the fragment,
-     * @param container          If non-null, this is the parent view that the fragment's
-     *                           UI should be attached to.  The fragment should not add the view itself,
-     *                           but this can be used to generate the LayoutParams of the view.
-     * @param savedInstanceState If non-null, this fragment is being re-constructed
-     *                           from a previous saved state as given here.
-     * @return Return the View for the fragment's UI, or null.
-     */
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.dialog_add, container, false);
     }
 
-    /**
-     * Called immediately after {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}
-     * has returned, but before any saved state has been restored in to the view.
-     * This gives subclasses a chance to initialize themselves once
-     * they know their view hierarchy has been completely created.  The fragment's
-     * view hierarchy is not however attached to its parent at this point.
-     *
-     * @param view               The View returned by {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
-     * @param savedInstanceState If non-null, this fragment is being re-constructed
-     */
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mBtnClose= (ImageButton) view.findViewById(R.id.btn_close);
-        mInputWhat= (EditText) view.findViewById(R.id.et_drop);
-        mInputWhen= (DatePicker) view.findViewById(R.id.bpv_date);
-        mBtnAdd= (Button) view.findViewById(R.id.btn_add);
+        mBtnClose = (ImageButton) view.findViewById(R.id.btn_close);
+        mInputWhat = (EditText) view.findViewById(R.id.et_drop);
+        mInputWhen = (DatePicker) view.findViewById(R.id.bpv_date);
+        mBtnAdd = (Button) view.findViewById(R.id.btn_add_it);
 
         mBtnClose.setOnClickListener(mBtnClickListener);
         mBtnAdd.setOnClickListener(mBtnClickListener);
-
     }
 }
